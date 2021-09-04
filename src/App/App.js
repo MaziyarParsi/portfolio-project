d8b513a8a4f798d5d7395413006b1a64514aef13
maziyar parsi <mparsi1992@gmail.com>
2021-09-04 23:49:40 +0430

    add-initial-hero-section

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Header from "../Header/Header"
+import HeroSection from "../HeroSection/HeroSection"
 import "./App.css"
 
 function App() {
   return (
     <div>
       <Header />
+      <HeroSection />
     </div>
   )
 }
---
+++ src/HeroSection/HeroSection.js
@@ -0,0 +1,16 @@
+import React from "react"
+import "./HeroSection.css"
+
+const HeroSection = () => (
+  <div className="heroSection">
+    <div className="heroContainer">
+      <div className="content">
+        <div className="text1">سلام، به وبسایت شخصی خوش اومدید. اسم من</div>
+        <div className="text2">مازیار پارسی</div>
+        <div className="text3">و کار من</div>
+      </div>
+    </div>
+  </div>
+)
+
+export default HeroSection
