8dca8b27fed596ecb1f3bb360f98c1b8ef249b68
maziyar parsi <mparsi1992@gmail.com>
2021-09-04 18:30:44 +0430

    add-initial-header-component

---
+++ src/Header/Header.js
@@ -0,0 +1,17 @@
+import React from "react"
+import "./Header.css"
+
+const Header = () => (
+  <div className="headerDiv">
+    <div>
+      <ul>
+        <li>خانه</li>
+        <li>درباره من</li>
+        <li>خدمات</li>
+        <li>مهارت ها</li>
+      </ul>
+    </div>
+  </div>
+)
+
+export default Header
