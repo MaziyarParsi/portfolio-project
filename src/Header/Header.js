e39494873acc921ac5337ad7d86428ce7806ef09
maziyar parsi <mparsi1992@gmail.com>
2021-09-05 16:42:59 +0430

    edit-header-section

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,13 +1,17 @@
 import React from "react"
 import "./Header.css"
-import Navbar from "../Navbar/Navbar"
 import Logo from "../Logo/Logo"
 
 const Header = () => (
   <div className="headerDiv">
     <div className="container">
       <Logo />
-      <Navbar />
+      <ul className="headerMenu">
+        <li>خدمات</li>
+        <li>مهارت ها</li>
+        <li>درباره من</li>
+        <li>خانه</li>
+      </ul>
     </div>
   </div>
 )
