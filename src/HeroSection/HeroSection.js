3c5a2057191f57f7b9cdcd6a569dc17c5d3fde3d
maziyar parsi <mparsi1992@gmail.com>
2021-09-05 17:13:55 +0430

    complete-hero-section

--- src/HeroSection/HeroSection.js
+++ src/HeroSection/HeroSection.js
@@ -1,16 +1,22 @@
 import React from "react"
 import "./HeroSection.css"
+import heroSectionImage from "../images/hero-section.png"
 
 const HeroSection = () => (
-  <div className="heroSection">
-    <div className="heroContainer">
-      <div className="content">
+  <section className="heroSection">
+    <div className="container">
+      <div>
+        <img src={heroSectionImage} alt="heroSectionImage" className="heroSectionImage"></img>{" "}
+      </div>
+      <div className="homeContent">
         <div className="text1">سلام، به وبسایت شخصی خوش اومدید. اسم من</div>
         <div className="text2">مازیار پارسی</div>
-        <div className="text3">و کار من</div>
+        <div className="text3">
+          و کار من <span>طراحی سایت</span>
+        </div>
       </div>
     </div>
-  </div>
+  </section>
 )
 
 export default HeroSection
