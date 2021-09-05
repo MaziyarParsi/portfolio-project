93b10477eb8daa58088a949d3445c708910ddb26
maziyar parsi <mparsi1992@gmail.com>
2021-09-05 22:02:12 +0430

    edit-hero-section

--- src/HeroSection/HeroSection.js
+++ src/HeroSection/HeroSection.js
@@ -1,19 +1,14 @@
 import React from "react"
 import "./HeroSection.css"
-import heroSectionImage from "../images/hero-section.png"
+import HeroSectionImage from "../HeroSectionBG/HeroSectionImage"
+import HeroSectionText from "../HeroSectionText/HeroSectionText"
 
 const HeroSection = () => (
   <section className="heroSection">
     <div className="container">
-      <div>
-        <img src={heroSectionImage} alt="heroSectionImage" className="heroSectionImage"></img>{" "}
-      </div>
       <div className="homeContent">
-        <div className="text1">سلام، به وبسایت شخصی خوش اومدید. اسم من</div>
-        <div className="text2">مازیار پارسی</div>
-        <div className="text3">
-          و کار من <span>طراحی سایت</span>
-        </div>
+        <HeroSectionImage />
+        <HeroSectionText />
       </div>
     </div>
   </section>
---
+++ src/HeroSectionText/HeroSectionText.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./HeroSectionText.css"
+
+const HeroSectionText = () => (
+  <div className="textArea">
+    <div className="text1">سلام، به وبسایت شخصی خوش اومدید. اسم من</div>
+    <div className="text2">مازیار پارسی</div>
+    <div className="text3">
+      و کار من <span>طراحی سایت</span>
+    </div>
+  </div>
+)
+
+export default HeroSectionText
