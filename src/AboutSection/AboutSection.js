6c59fd1a704c9af189042278f7b7fb6d7298d479
maziyar parsi <mparsi1992@gmail.com>
2021-09-06 18:19:38 +0430

    add-AboutSection-files

---
+++ src/AboutSection/AboutSection.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./AboutSection.css"
+
+const AboutSection = () => {
+  return (
+    <section className="heroSection">
+      <div className="container">
+        <HeroSectionImage />
+        <HeroSectionText />
+      </div>
+    </section>
+  )
+}
+export default AboutSection
