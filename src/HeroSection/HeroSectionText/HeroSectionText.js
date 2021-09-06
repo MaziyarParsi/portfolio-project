ce4470c955a438517bdc8a99c9ef89f15831eaec
maziyar parsi <mparsi1992@gmail.com>
2021-09-06 17:21:17 +0430

    make-typing-effect

--- src/HeroSection/HeroSectionText/HeroSectionText.js
+++ src/HeroSection/HeroSectionText/HeroSectionText.js
@@ -1,14 +1,24 @@
 import React from "react"
 import "./HeroSectionText.css"
+import Typical from "react-typical"
 
-const HeroSectionText = () => (
-  <div className="textArea">
-    <div className="text1">سلام، به وبسایت شخصی خوش اومدید. اسم من</div>
-    <div className="text2">مازیار پارسی</div>
-    <div className="text3">
-      و کار من <span>طراحی سایت</span>
+const HeroSectionText = () => {
+  return (
+    <div className="textArea">
+      <div className="text1">سلام، به وبسایت شخصی خوش اومدید. اسم من</div>
+      <div className="text2">مازیار پارسی</div>
+      <div className="text3">
+        <p>
+          و کار من{" "}
+          <Typical
+            className="typical"
+            wrapper="p"
+            loop={Infinity}
+            steps={["طراح سایت", 1000, "برنامه نویس وب", 1000, "طراح وردپرس", 1000]}
+          />
+        </p>
+      </div>
     </div>
-  </div>
-)
-
+  )
+}
 export default HeroSectionText
